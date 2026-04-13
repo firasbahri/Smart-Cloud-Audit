d72190dd6d1f518a2d03b0bd1ba9b2d17d8fb908
firasbahri <firasbahri@usal.es>
2026-04-13 18:18:37 +0100

    update ec2_analyzer test

--- Backend/tests/test_ec2_analyzer.py
+++ Backend/tests/test_ec2_analyzer.py
@@ -10,9 +10,9 @@ def make_instance(id,volumes):
 
 def test_check_ebs_encryption():
     analyzer = EC2Analyzer()
-    instance1 = make_instance("i-1234567890abcdef0", [{"Encrypted": False}])
-    instance2= make_instance("i-0987654321abcdef0", [{"Encrypted": True}])
-    instance3= make_instance("i-1122334455667788", [{"Encrypted": False}, {"Encrypted": True}])
+    instance1 = make_instance("i-1234567890abcdef0", [{"VolumeId": "vol-1234567890abcdef0", "Encrypted": False}])
+    instance2 = make_instance("i-0987654321abcdef0", [{"VolumeId": "vol-0987654321abcdef0", "Encrypted": True}])
+    instance3 = make_instance("i-1122334455667788", [{"VolumeId": "vol-1122334455667788", "Encrypted": False}, {"VolumeId": "vol-1122334455667789", "Encrypted": True}])
 
     instances = [instance1, instance2, instance3]
     vulnerabilities = analyzer.check_ebs_encryption(instances)
