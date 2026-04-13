73e42161c721b60e4bc26204f3bd4391bb68299f
firasbahri <firasbahri@usal.es>
2026-04-13 02:22:22 +0100

    change mail_port

--- Backend/services/email_service.py
+++ Backend/services/email_service.py
@@ -6,10 +6,10 @@ conf=ConnectionConfig(
     MAIL_USERNAME = os.getenv("MAIL_USERNAME"),
     MAIL_PASSWORD = os.getenv("MAIL_PASSWORD"),
     MAIL_FROM = os.getenv("MAIL_FROM"),
-    MAIL_PORT = int(os.getenv("MAIL_PORT")),
-    MAIL_SERVER = os.getenv("MAIL_SERVER"),
-    MAIL_STARTTLS = True,
-    MAIL_SSL_TLS = False
+    MAIL_PORT = 465,
+    MAIL_SERVER = "smtp.gmail.com",
+    MAIL_STARTTLS = False,
+    MAIL_SSL_TLS = True
 )
 
 async def send_email(email: EmailStr, token: str):
