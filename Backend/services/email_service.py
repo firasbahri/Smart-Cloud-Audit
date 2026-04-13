e8e3a1d322a9e2bd33ea060f589a40ecfd9231c4
firasbahri <firasbahri@usal.es>
2026-04-13 02:31:23 +0100

    use resend

--- Backend/services/email_service.py
+++ Backend/services/email_service.py
@@ -1,27 +1,20 @@
-from fastapi_mail import FastMail, MessageSchema, ConnectionConfig
-from pydantic import  EmailStr
+import resend
 import os
 
-conf=ConnectionConfig(
-    MAIL_USERNAME = os.getenv("MAIL_USERNAME"),
-    MAIL_PASSWORD = os.getenv("MAIL_PASSWORD"),
-    MAIL_FROM = os.getenv("MAIL_FROM"),
-    MAIL_PORT = 465,
-    MAIL_SERVER = "smtp.gmail.com",
-    MAIL_STARTTLS = False,
-    MAIL_SSL_TLS = True
-)
+resend.api_key = os.getenv("RESEND_API_KEY")
 
-async def send_email(email: EmailStr, token: str):
-    server_host=os.getenv("SERVER_HOST")
-    link= f"{server_host}/auth/verify-email?token={token}"
-    body = f"Please click the following link to verify your email: <a href='{link}'>Verify Email</a>"
-    subject="SmartAudit - Email Verification"
-    message = MessageSchema(
-        subject=subject,
-        recipients=[email],
-        body=body,
-        subtype="plain"
-    )
-    fm = FastMail(conf)
-    await fm.send_message(message)
+async def send_email(email: str, token: str):
+    server_host = os.getenv("SERVER_HOST")
+    link = f"{server_host}/auth/verify-email?token={token}"
+    body = f"""
+        <h2>SmartAudit - Verificación de Email</h2>
+        <p>Haz clic en el siguiente enlace para verificar tu email:</p>
+        <a href='{link}'>Verificar Email</a>
+    """
+
+    resend.Emails.send({
+        "from": os.getenv("MAIL_FROM"),
+        "to": email,
+        "subject": "SmartAudit - Email Verification",
+        "html": body
+    })
